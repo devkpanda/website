b52223dceea121d7dd12aabd6afc7a274c1f2832
ghriphos <luisfernando._alves@hotmail.com>
2023-03-24 16:39:07 -0300

    feat: add pages contato and produtos to index page

--- assets/js/websiteController.js
+++ assets/js/websiteController.js
@@ -1,13 +1,74 @@
 $(document).ready(function () {
-    $("#initialIndex").show()
+    // makes the initial index
+    $("#index").show()
     $("#sobre").hide()
+    $("#contato").hide()
+    $("#produtos").hide()
+    $("#carrinhoUtils").hide()
     $("#navigationBar").hide()
+    $("#footer").hide()
 
-    $("#indexButton").click(function () {
-        $("#initialIndex").hide()
-        $("#navigationBar").show()
-        $("#sobre").fadeIn("slow")
+    $("#logoButton").click(function () {
+        $("#navigationBar").hide()
+        $("#index").fadeIn("slow")
+        $("#sobre").hide()
+        $("#contato").hide()
+        $("#footer").hide()
     })
 
-    
+    $("#entrarEmContatoButton").click(function () {
+        $("navigationBar").show()
+        $("#index").hide()
+        $("#sobre").hide()
+        $("#contato").fadeIn("slow")
+        $("#footer").show()
+    })
+
+    // when click indexButton, show About page
+    $("#indexButton").click(function () {
+        $("#navigationBar").show()
+        $("#index").hide()
+        $("#sobre").fadeIn("slow")
+        $("#contato").hide()
+        $("#produtos").hide()
+        $("#carrinhoUtils").hide()
+        $("#footer").show()
+    })
+
+    $("#contatoButton").click(function () {
+        $("navigationBar").show()
+        $("#index").hide()
+        $("#sobre").hide()
+        $("#contato").fadeIn("slow")
+        $("#produtos").hide()
+        $("#carrinhoUtils").hide()
+        $("#footer").show()
+    })
+
+    $("#sobreButton").click(function () {
+        $("navigationBar").show()
+        $("#index").hide()
+        $("#sobre").fadeIn("slow")
+        $("#contato").hide()
+        $("#produtos").hide()
+        $("#carrinhoUtils").hide()
+        $("#footer").show()
+
+        $("#ceciliaInfo").hide()
+        $("#luisInfo").hide()
+        $("#richardInfo").hide()
+        $("#jhonnataInfo").hide()
+        $("#vitorInfo").hide()
+    })
+
+    $("#servicosButton").click(function () {
+        $("navigationBar").show()
+        $("#index").hide()
+        $("#sobre").hide()
+        $("#contato").hide()
+        $("#produtos").fadeIn("slow")
+        $("#carrinhoUtils").fadeIn("slow")
+        $("#footer").show()
+    })
+
 })
