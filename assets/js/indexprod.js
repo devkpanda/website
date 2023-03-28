146ca49d0d8481c9aa452395d99c63e2dddb2c8a
ceciliascm <ceciliasoarescosta@gmail.com>
2023-03-27 22:41:27 -0300

    mudando título dos cards

--- assets/js/indexprod.js
+++ assets/js/indexprod.js
@@ -3,11 +3,14 @@
 $(document).ready(function () {
     $("#produtos2").hide();
     $("#produtos3").hide();
+    $("#produtos4").hide();
+
 
     $("#button1").click(function () {
         $("#produtos1").fadeIn("slow");
         $("#produtos2").hide();
         $("#produtos3").hide();
+        $("#produtos4").hide();
 
         // $("#textolateral").css('margin-top', '2%')
     });
@@ -16,6 +19,7 @@ $(document).ready(function () {
         $("#produtos1").hide();
         $("#produtos3").hide();
         $("#produtos2").fadeIn("slow");
+        $("#produtos4").hide();
         // $("#textolateral").css('margin-top', '5%') 
     });
 
@@ -23,7 +27,17 @@ $(document).ready(function () {
         $("#produtos3").fadeIn("slow");
         $("#produtos1").hide();
         $("#produtos2").hide();
+        $("#produtos4").hide();
         // $("#textolateral").css('margin-top', '2%')
     });
+
+    $("#button4").click(function () {
+        $("#produtos4").fadeIn("slow");
+        $("#produtos1").hide();
+        $("#produtos2").hide();
+        $("#produtos3").hide();
+        // $("#textolateral").css('margin-top', '2%')
+    });
+
 
 });
